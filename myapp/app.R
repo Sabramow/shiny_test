94e92cccd525afa99a03ee4b88c92a380d24f8dc
Sarah Abramowitz <sabramow@roubaix.pmacs.upenn.edu>
2023-11-09 15:26:35 -0500

    testing123

--- myapp/app.R
+++ myapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data, Sarah's version"),
+    titlePanel("Old Faithful Geyser Data, Sarah's version. Testing 123"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
